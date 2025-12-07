a231394d5309b71519d49df7c1311c256d4c780e
meanev56 <emekanjoku86@gmail.com>
2025-12-07 13:14:39 +0100

    feat: implement display handling with Welcome component in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <script setup>
   import Welcome from './components/pages/Welcome.vue';
   import Layout from './components/layouts/Layout.vue';
+
+  import { ref } from 'vue';
+
+
+
+  const selectedDisplay = ref(1);
+
+
+  function handleChangeDisplay(idx) {
+    selectedDisplay.value = idx
+  }
+
 </script>
 
 <template>
   <Layout>
+
+    <!-- page 1 -->
+    <Welcome :handleChangeDisplay="handleChangeDisplay" v-if="selectedDisplay == 1"  />
 
   </Layout>
   
